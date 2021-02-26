8b9dbf73680b0f6601e818bc00091ceeec28c556
dinafi <dinafi@upi.edu>
2021-02-26 09:57:50 +0700

    Aktifasi

---
+++ aktifasi1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("Masukan Nama Anda : ")
+umur=19
+
+#OUTPUT
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHASA PEMOGRAMAN PYTHON=====")
+print("Nama Anda : ")
+print("Umur Anda : ")
+print("")
---
+++ aktifasi3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN
+print("Percabangan Pada Bahasa Pemograman Python")
+print("Pilih Bahasa Pemograman : ")
+print("1.Python")
+print("2. C")
+
+pilih=int(input("Pilih Menu (1/2) : ))
+                
+if pilih == 1:
+    print("Bahasa Pemograman yang Dipilih Adalah Python")
+elif pilih == 2:
+    print("Bahasa Pemograman yang Dipilih Adalah C")
+elif :
+    print("Pilihan Anda Tidak Tersedia!!!")
